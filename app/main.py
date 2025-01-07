192ad41c196c6df04e6856bae34cb5dcc2f0485c
Harshit Sharma <harshit158@gmail.com>
2025-01-06 20:00:25 -0500

    udpate

--- app/main.py
+++ app/main.py
@@ -6,11 +6,21 @@ def main():
     while True:
         sys.stdout.write("$ ")
         command = input()
-        if command.startswith("exit"):
+        if command.startswith("exit "):
             sys.exit(0)
-        elif command.startswith("echo"):
-            command_args = ' '.join(command.split()[1:])
-            sys.stdout.write(f"{command_args}\n")
+            
+        elif command.startswith("echo "):
+            args = ' '.join(command.split()[1:])
+            sys.stdout.write(f"{args}\n")
+            
+        elif command.startswith("type "):
+            arg = command.split()[1]
+            
+            if arg in ["echo", "exit", "type"]:
+                sys.stdout.write(f"{arg} is a shell builtin\n")
+            else:
+                sys.stdout.write(f"{arg}: not found\n")    
+                
         else:
             sys.stdout.write(f"{command}: command not found\n")
 
